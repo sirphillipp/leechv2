8a9df1ad7efa4653cb6619be1e6803de1121bad7
Nick Fury <73124813+NickxFury@users.noreply.github.com>
2021-05-15 17:37:34 +0530

    Update display_progress.py

--- tobrot/helper_funcs/display_progress.py
+++ tobrot/helper_funcs/display_progress.py
@@ -77,18 +77,18 @@ class Progress:
             elapsed_time = TimeFormatter(milliseconds=elapsed_time)
             estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-            progress = "<b>╭────── ⌊__UploadinG: 〘{2}%〙 📤__⌉</b>\n│ \n<b>├〖{0}{1}〗</b>\n".format(
+            progress = "<b>╔═══════════════ ⌊__𝗨𝗽𝗹𝗼𝗮𝗱𝗶𝗻𝗴: {2}  📤__⌉</b>\n║ \n<b>╠═ {0}{1} </b>\n".format(
                 ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 5))]),
                 ''.join([UN_FINISHED_PROGRESS_STR for i in range(20 - math.floor(percentage / 5))]),
                 round(percentage, 2))
             #cpu = "{psutil.cpu_percent()}%"
-            tmp = progress +"│" + "\n**├Total 📀:**   〘<code>{1}</code>〙\n**├Done ✓ :** 〘<code>{0}</code>〙\n**├Speed** 🚀 :  〘<code>{2}/s</code>〙\n**├ETA** ⏳ :  〘<code>{3}</code>〙\n**│**\n**╰── ⌊ @mirrordev ⌉**".format(
+            tmp = progress +"║" + "\n**╠═💾 𝗧𝗼𝘁𝗮𝗹 𝗦𝗶𝘇𝗲:**   <code>{1}</code> \n**╠═🕐 𝗖𝗼𝗺𝗽𝗹𝗲𝘁𝗲𝗱 ** :  <code>{0}</code> \n**╠═🧭 𝗦𝗽𝗲𝗲𝗱 ** : <code>{2}/s</code> \n**╠═⏰ 𝗘𝗧𝗔** :  <code>{3}</code> \n**║**\n**╚══ ⌊ 𝗗𝗘𝗩 𝗖𝗟𝗢𝗨𝗗 ⌉**".format(
                 humanbytes(current),
                 humanbytes(total),
                 humanbytes(speed),
                 # elapsed_time if elapsed_time != '' else "0 s",
                 estimated_total_time if estimated_total_time != "" else "0 s",
-            #tmp += "\n│"+"\n╰── ⌊ @mirrordev ⌉"
+            #tmp += "\n│"+"\n╚══ ⌊ 𝗗𝗘𝗩 𝗖𝗟𝗢𝗨𝗗 ⌉"
             )
             try:
                 if not self._mess.photo:
