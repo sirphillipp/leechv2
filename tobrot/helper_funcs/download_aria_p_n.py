4c9097a71b33808d3a9e0ce74f9d2e72a23fb1c8
Nick Fury <73124813+NickxFury@users.noreply.github.com>
2021-05-15 22:18:09 +0530

    Update download_aria_p_n.py

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -326,20 +326,15 @@ async def check_progress_for_dl(aria2, gid, event, previous_message):
                 if is_file is None:
                     msgg = f"<b>╠═ ⚙ 𝗖𝗼𝗻𝗻𝗲𝗰𝘁𝗶𝗼𝗻𝘀 : {file.connections} </b>"
                 else:
-                msg = f"<b>╔═══════════════ ⌊ 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱𝗶𝗻𝗴 : [ {2} ] 📥 ⌉</b>\n║ \n<b>╠═ 〚 {0}{1} 〛 </b>\n".format(
-                ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 5))]),
-                ''.join([UN_FINISHED_PROGRESS_STR for i in range(20 - math.floor(percentage / 5))]),
-                round(percentage, 2))
-              #cpu = "{psutil.cpu_percent()}%"
-                  msgg = f"<b>╠═ Info :- P: {file.connections} || S: {file.num_seeders} </b>\n\n<b>🗑️ GID :</b> <code>{gid}</code>"
-                msg = f"\n<b> ╠═ 📀 𝗙𝗶𝗹𝗲 𝗡𝗮𝗺𝗲:</b> `{downloading_dir_name}`\n\n<b>╠═ 🧭 𝗦𝗽𝗲𝗲𝗱 :</b> `{file.download_speed_string()}`"
-                msg += f"\n<b>╠═ 💾 𝗧𝗼𝘁𝗮𝗹 𝗦𝗶𝘇𝗲 :</b> `{file.total_length_string()}`"
-                msg += f"\n<b>╠═ ⏳𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱𝗲𝗱</b> : `{file.progress_string()}` \n<b>╠═ ⏰ 𝗘𝗧𝗔 :</b> `{file.eta_string()}` \n {msgg}"
-                msg += f"\n<b>╚══ ⌊ ⚡️ 𝗗𝗘𝗩 𝗖𝗟𝗢𝗨𝗗 ⌉"
+                    msgg = f"<b>➩ Info :- P: {file.connections} || S: {file.num_seeders} </b>\n\n<b>🗑️ GID :</b> <code>{gid}</code>"
+                msg = f"\n<b> ╔═══════════════ ⌊  📥  𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱𝗶𝗻𝗴.. ⌉ "
+                msg = f"\n<b> ╠═📘 File Name :</b> `{downloading_dir_name}`\n\n<b>╠═ Speed :</b> `{file.download_speed_string()}`"
+                msg += f"\n<b>╠═ Size :</b> `{file.total_length_string()}`"
+                msg += f"\n<b>╠═ Downloaded</b> : `{file.progress_string()}` \n<b>╠═ ETA :</b> `{file.eta_string()}` \n {msgg}"
+                msg += f"\n<b>╚════ ⌊⚡️ 𝗗𝗘𝗩 𝗖𝗟𝗢𝗨𝗗 ⌉"
                 inline_keyboard = []
                 ikeyboard = []
-                ikeyboard.append(
-                    InlineKeyboardButton(
+                ikeyboard.append(   InlineKeyboardButton(
                         "❌ Cancel ❌", callback_data=(f"cancel {gid}").encode("UTF-8")
                     )
                 )
