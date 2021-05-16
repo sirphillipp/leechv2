88162c5ac801935ee74c723d1d2dac2e4d55ffb4
Nick Fury <73124813+NickxFury@users.noreply.github.com>
2021-05-16 12:31:12 +0530

    Update download_aria_p_n.py

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Shrimadhav U K | gautamajay52 | MaxxRider | NickxFury
+# (c) Shrimadhav U K | gautamajay52 | MaxxRider
 
 import asyncio
 import logging
